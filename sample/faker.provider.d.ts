f1332efc72dbcc882bac6655c30b5863be2ed37e
omermorad <omer.moradd@gmail.com>
2021-11-23 08:24:51 +0200

    remove sample build

--- sample/faker.provider.d.ts
+++
@@ -1,3 +0,0 @@
-import * as faker from 'faker';
-import { ValueProvider } from '@nestjs/common';
-export declare const FakerProvider: ValueProvider<typeof faker>;
--- sample/random-name.service.d.ts
+++
@@ -1,6 +0,0 @@
-import FakerStatic = Faker.FakerStatic;
-export declare class RandomNameService {
-    private readonly faker;
-    constructor(faker: FakerStatic);
-    getRandomName(): string;
-}

c346649e906b5269978cbc8aa4c3487eca557f5f
omermorad <omer.moradd@gmail.com>
2021-12-05 20:08:41 +0200

    chore: double comment

--- packages/reflect/src/lib/agnostic-class-reflector.ts
+++ packages/reflect/src/lib/agnostic-class-reflector.ts
@@ -1,14 +1,14 @@
 import { ClassReflectorAbstract } from './class-reflector-abstract';
 import { Type } from '@automock/common';
 
-// export class AgnosticClassReflector extends ClassReflectorAbstract {
-// protected readonly CUSTOM_METADATA: string;
-// protected readonly PARAMTYPES_METADATA: string;
-
-// public constructor(reflector: typeof Reflect, targetClass: Type<unknown>) {
-//   super(reflector, targetClass);
-// }
+// // export class AgnosticClassReflector extends ClassReflectorAbstract {
+// // protected readonly CUSTOM_METADATA: string;
+// // protected readonly PARAMTYPES_METADATA: string;
 //
-// protected reflectDependencies(): any {}
-// }
-//
+// // public constructor(reflector: typeof Reflect, targetClass: Type<unknown>) {
+// //   super(reflector, targetClass);
+// // }
+// //
+// // protected reflectDependencies(): any {}
+// // }
+// //

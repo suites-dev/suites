e39a2b86392efdfd8ea32f50fcbb5a5d98ae2178
omermorad <omer.moradd@gmail.com>
2023-02-04 16:28:12 +0200

    fix

--- jest.base.config.ts
+++ jest.base.config.ts
@@ -2,16 +2,25 @@ import type { Config } from '@jest/types';
 
 export default async (coverageFileName = 'coverage-report'): Promise<Config.InitialOptions> => {
   return {
-    moduleFileExtensions: ['js', 'json', 'ts'],
+    preset: 'ts-jest',
+    roots: ['<rootDir>/src', '<rootDir>/test'],
     rootDir: '.',
+    moduleFileExtensions: ['js', 'json', 'ts'],
     testRegex: '.(spec|test).ts$',
     transform: {
       '^.+\\.(t|j)s$': 'ts-jest',
     },
     coveragePathIgnorePatterns: ['/node_modules/', 'spec-assets.ts', 'spec-assets.js'],
-    testPathIgnorePatterns: ['/node_modules/', 'sample'],
+    testPathIgnorePatterns: ['/node_modules/'],
     coverageDirectory: './coverage',
-    coverageReporters: ['text', 'cobertura'],
+    coverageReporters: ['text', ['cobertura', { file: `${coverageFileName}.xml` }]],
     testEnvironment: 'node',
+    reporters: ['default', 'jest-junit'],
+    testResultsProcessor: 'jest-junit',
+    globals: {
+      'ts-jest': {
+        isolatedModules: true,
+      },
+    },
   };
 };

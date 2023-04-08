9ba7f360804be180993d706cc5eb2e51b92ce5da
Omer Morad <omer.moradd@gmail.com>
2023-04-08 08:21:07 +0300

    feat(types): add scratch types package (#34)

---
+++ packages/types/src/index.ts
@@ -0,0 +1,15 @@
+export type DeepPartial<Type> = {
+  [Key in keyof Type]?: Type[Key] extends Array<infer U>
+    ? Array<DeepPartial<U>>
+    : Type[Key] extends ReadonlyArray<infer U>
+    ? ReadonlyArray<DeepPartial<U>>
+    : unknown extends Type[Key]
+    ? Type[Key]
+    : DeepPartial<Type[Key]>;
+};
+
+export type FnPartialReturn<Type> = {
+  [Key in keyof Type]?: Type[Key] extends (...args: infer Args) => infer U
+    ? (...args: Args) => FnPartialReturn<U>
+    : DeepPartial<Type[Key]>;
+};

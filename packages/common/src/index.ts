5524a8511aa34fd61fc2943492b500a7e0513bd1
omermorad <omer.moradd@gmail.com>
2023-07-01 00:05:58 +0300

    refactor(common): change dependencies reflector signature
    Refactor the `DependenciesReflector` in the common package to change its signature and return a different interface. This change was made to support both constructor parameters and properties. The motivation behind this change is to address a bug in the reflector where returning a Map does not allow dependencies to appear more than once.
    
    Closes: https://github.com/automock/automock/issues/67
    
    Please note that this is a breaking change.
    BREAKING CHANGE: The `reflectDependencies` method of `DependenciesReflector` now returns a different interface to accommodate the support for both constructor parameters and properties.

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -2,8 +2,12 @@ import { Type } from '@automock/types';
 
 export type PrimitiveValue = string | number | boolean | symbol | null;
 
-export type ClassDependencies = Map<Type | string, Type | PrimitiveValue>;
+export type ClassDependencies = [Type | string, PrimitiveValue | Type][];
+
+export interface ClassDependenciesMap {
+  constructor: ClassDependencies;
+}
 
 export interface DependenciesReflector {
-  reflectDependencies(targetClass: Type): ClassDependencies;
+  reflectDependencies(targetClass: Type): ClassDependenciesMap;
 }

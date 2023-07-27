68618e16a95ada2a9334879cfe392ffbb05a4593
omermorad <omer.moradd@gmail.com>
2023-07-27 17:56:10 +0300

    feat(common): add `properties` prop to dependencies map interface

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -2,10 +2,41 @@ import { Type } from '@automock/types';
 
 export type PrimitiveValue = string | number | boolean | symbol | null;
 
+/**
+ * @deprecated
+ * Use `ClassCtorInjectables` instead
+ */
 export type ClassDependencies = [Type | string, PrimitiveValue | Type][];
 
+/**
+ * @since 2.1.0
+ */
+export type ClassCtorInjectables = ClassDependencies;
+
+/**
+ * @since 2.1.0
+ */
+export interface ClassInjectableProperty {
+  property: string;
+  typeOrToken: Type | string;
+  value?: PrimitiveValue | Type;
+}
+
+/**
+ * @since 2.1.0
+ */
+export type ClassPropsInjectables = ClassInjectableProperty[];
+
 export interface ClassDependenciesMap {
-  constructor: ClassDependencies;
+  /**
+   * @since 2.0.0
+   */
+  constructor: ClassCtorInjectables;
+
+  /**
+   * @since 2.1.0
+   */
+  properties: ClassPropsInjectables;
 }
 
 export interface DependenciesReflector {

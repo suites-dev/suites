4bc96e20c3ba54aabd23dbe6d09e0074e424b4c8
omer <omer.moradd@gmail.com>
2023-11-01 00:16:28 +0200

    refactor(jest): improve interfaces and remove types
    - Revise the `UnitReference` interface to:
      - Support symbols as identifiers.
      - Provide more accurate generic parameters.
    
    - Relocate core types for better organization:
      - Move `Type` to `@automock/types`.
      - Transfer `UnitTestBed` to `@automock/core`.
    
    - Simplify and declutter:
      - Introduce a new, streamlined `UnitReference` interface.
      - Remove redundant types: `Override` and `TestBedResolver`.
    
    BREAKING CHANGE:
    - Users will need to update their imports due to the relocation of `Type` and `UnitTestBed`.
    - Adjustments may be required for the new `UnitReference` interface.

--- packages/testbeds/jest/src/index.ts
+++ packages/testbeds/jest/src/index.ts
@@ -1,69 +1,62 @@
 /// <reference types="jest" />
 
-import { Type as TypeFromTypes } from '@automock/types';
-import { MockOverride, TestBedBuilder, UnitReference } from '@automock/core';
-
+import { Type } from '@automock/types';
 export * from './testbed-factory';
-
-/**
- * @deprecated Use `TestBedBuilder` from `@automock/core` instead of `TestBedResolver` from `@automock/jest`.
- */
-export type TestBedResolver<TClass> = TestBedBuilder<TClass>;
-
-export type Type<T> = TypeFromTypes<T>;
 
 declare module '@automock/core' {
   /**
-   * Represents a reference to the mocked object based on the provided type or token.
-   * This is an augmented module for the `StubbedInstance` type from @automock/core.
+   * Provides a reference to mock objects that have been mocked for testing
+   * purposes within the test environment.
+   *
+   * Augmentation of the `@automock/core` module for the `StubbedInstance` type.
+   * In this context, the `StubbedInstance` type is replaced by `jest.Mocked`.
+   *
+   * Essentially, when you retrieve an instance using this interface, you're not getting
+   * the original instance but a Jest-mocked version of it, allowing for enhanced testing
+   * capabilities like spying on method calls, faking return values, and more.
+   *
+   * @see https://automock.dev/api-reference/api/unitreference-api
    */
   export interface UnitReference {
     /**
-     * Returns a reference to the mocked object based on the provided class type.
+     * Retrieves a reference to the mocked object of a dependency corresponding to its type identifier.
      *
-     * @param type - The type of the dependency.
-     * @returns The mocked instance of the dependency.
-     * @template TDep - The type of the dependency.
+     * @template TDependency The type of the dependency being retrieved.
+     * @param type The type representing the dependency.
+     * @returns The mocked object corresponding to the provided type identifier.
      */
-    get<TDep>(type: Type<TDep>): jest.Mocked<TDep>;
+    get<TDependency>(type: Type<TDependency>): jest.Mocked<TDependency>;
 
     /**
-     * Returns a reference to the mocked object based on the provided token.
+     * Retrieves a reference to the mocked object of a dependency corresponding to a string-based token.
      *
-     * @param token - The token of the dependency.
-     * @returns The mocked instance of the dependency.
-     * @template TDep - The type of the dependency.
+     * @template TDependency The type of the dependency being retrieved.
+     * @param token The string-based token representing the dependency.
+     * @returns The mocked object corresponding to the provided string-based token.
      */
-    get<TDep>(token: string): jest.Mocked<TDep>;
+    get<TDependency>(token: string): jest.Mocked<TDependency>;
 
     /**
-     * Returns a reference to the mocked object based on the provided token or class type.
+     * Retrieves a reference to the mocked object of a dependency corresponding to a symbol-based token.
      *
-     * @param typeOrToken - The type or token of the dependency.
-     * @returns The mocked instance of the dependency.
-     * @template TDep - The type of the dependency.
+     * @since 2.0.0
+     * @template TDependency The type of the dependency being retrieved.
+     * @param token The symbol-based token representing the dependency.
+     * @returns The mocked object corresponding to the provided symbol-based token.
      */
-    get<TDep>(typeOrToken: Type<TDep> | string): jest.Mocked<TDep>;
+    get<TDependency>(token: symbol): jest.Mocked<TDependency>;
+
+    /**
+     * Retrieves a mocked object or a constant value of a dependency using its type, string, or symbol token.
+     *
+     * This method provides flexibility in retrieving dependencies by allowing various identifier types.
+     * Depending on the identifier and the setup, it can return either a mocked object or a constant value.
+     *
+     * @since 2.0.0
+     * @template TDependency The type of the dependency being retrieved.
+     * @param identifier The type or token that the dependency corresponds to.
+     * @returns The mocked object corresponding to the provided identifier.
+     */
+    get<TDependency>(identifier: Type<TDependency> | string | symbol): jest.Mocked<TDependency>;
   }
-}
-
-/**
- * @deprecated Use `MockOverride` from `@automock/core` instead.
- */
-export type Override<T> = MockOverride<any, T>;
-
-/**
- * Represents the result of compiling a unit test bed.
- * @template TClass - The type of the class under test.
- */
-export interface UnitTestBed<TClass> {
-  /**
-   * The instance of the class under test.
-   */
-  unit: TClass;
-
-  /**
-   * The reference to the dependencies of the class under test.
-   */
-  unitRef: UnitReference;
 }

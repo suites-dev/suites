12e4ad4c3f2911591cf104e8b6fdcba0e7d6c74d
omer <omer.moradd@gmail.com>
2023-12-04 08:47:03 +0200

    feat(adapters.nestjs): export identifier metadata to align with common
    Include `IdentifierMetadata` to prevent the adapter from using metadata

--- packages/adapters/nestjs/src/types.ts
+++ packages/adapters/nestjs/src/types.ts
@@ -4,6 +4,7 @@ export type ForwardRefToken = { forwardRef: () => InjectableReflectedType };
 export type NestInjectableIdentifier = ForwardRefToken | InjectableIdentifier;
 
 export type MetadataReflector = typeof Reflect;
+export type IdentifierMetadata = never;
 
 export interface ReflectedProperty {
   key: string;

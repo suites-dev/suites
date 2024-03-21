44baa0c06a05263bc1c1ce14b960c5b711b8e088
omermorad <omer.moradd@gmail.com>
2024-03-21 20:27:41 +0200

    chore: add post install scripts

---
+++ packages/doubles/jest/postinstall.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, '..', 'unit', 'typings.d.ts');
+const newContent = "export { TestBed } from './dist;'\nexport * from '@suites/doubles.jest';\n";
+
+fs.readFile(filePath, 'utf8', function (err, data) {
+  console.log('Suites: attempting to override @suites/unit typings file');
+
+  if (err) {
+    console.log('Suites: an error occurred while reading @suites/unit typings file');
+    return console.error(err);
+  }
+
+  const updatedData = data + newContent;
+
+  fs.writeFile(filePath, updatedData, 'utf8', function (err) {
+    if (err) {
+      console.log('Suites: an error occurred while writing to @suites/unit typings file');
+      return console.log(err);
+    }
+
+    console.log(
+      'Suites: @suites/unit typings file updated successfully with the proper doubles adapter (@suites/doubles.jest)'
+    );
+  });
+});

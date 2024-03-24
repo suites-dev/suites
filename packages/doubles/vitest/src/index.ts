c8b1757d6723195c0d797fb7f9f8f1148c9587e5
omermorad <omer.moradd@gmail.com>
2024-03-24 06:57:02 +0200

    doubles post install scripts, package.json and augmentation

--- packages/doubles/vitest/src/index.ts
+++ packages/doubles/vitest/src/index.ts
@@ -1,4 +1,3 @@
-import { vi } from 'vitest';
 import { Mocked as VitestMocked } from '@vitest/spy';
 import { UnitReference as CoreUnitReference } from '@suites/core.unit';
 import { Type } from '@suites/types.common';
@@ -9,7 +8,7 @@ import { mock } from './mock.static';
  * Provides a reference to mock objects that have been mocked for testing
  * purposes within the test environment.
  *
- * Augmentation of the `@suites/core` module for the `StubbedInstance` type.
+ * Augmentation of the `@suites/unit` module for the `StubbedInstance` type.
  * In this context, the `StubbedInstance` type is replaced by `Mocked`.
  *
  * Essentially, when you retrieve an instance using this interface, you're not getting
@@ -18,109 +17,110 @@ import { mock } from './mock.static';
  *
  * @see https://suites.dev/api-reference/api/unitreference-api
  */
-export interface UnitReference extends CoreUnitReference {
-  /**
-   * Retrieves a reference to the mocked object of a dependency corresponding to its type
-   * identifier.
-   *
-   * @template TDependency The type of the dependency being retrieved.
-   * @param type The type representing the dependency.
-   * @returns The mocked object corresponding to the provided type identifier.
-   */
-  get<TDependency>(type: Type<TDependency>): Mocked<TDependency>;
+declare module '@suites/unit' {
+  export interface UnitReference extends CoreUnitReference {
+    /**
+     * Retrieves a reference to the mocked object of a dependency corresponding to its type
+     * identifier.
+     *
+     * @template TDependency The type of the dependency being retrieved.
+     * @param type The type representing the dependency.
+     * @returns The mocked object corresponding to the provided type identifier.
+     */
+    get<TDependency>(type: Type<TDependency>): Mocked<TDependency>;
 
-  /**
-   * Retrieves a reference to the mocked object of a dependency corresponding to its
-   * type identifier and the identifier metadata.
-   *
-   * @since 2.1.0
-   * @template TDependency The type of the dependency being retrieved.
-   * @param type The type representing the dependency
-   * @param identifierMetadata An accompanying metadata object for the token identifier.
-   * @returns Mocked<TDependency> The mocked object corresponding to the provided
-   * type identifier and identifier metadata.
-   */
-  get<TDependency>(
-    type: Type<TDependency>,
-    identifierMetadata: IdentifierMetadata
-  ): VitestMocked<TDependency>;
+    /**
+     * Retrieves a reference to the mocked object of a dependency corresponding to its
+     * type identifier and the identifier metadata.
+     *
+     * @since 2.1.0
+     * @template TDependency The type of the dependency being retrieved.
+     * @param type The type representing the dependency
+     * @param identifierMetadata An accompanying metadata object for the token identifier.
+     * @returns Mocked<TDependency> The mocked object corresponding to the provided
+     * type identifier and identifier metadata.
+     */
+    get<TDependency>(
+      type: Type<TDependency>,
+      identifierMetadata: IdentifierMetadata
+    ): VitestMocked<TDependency>;
 
-  /**
-   * Retrieves a reference to the mocked object of a dependency corresponding to a string-based
-   * token.
-   *
-   * @template TDependency The type of the dependency being retrieved.
-   * @param token The string-based token representing the dependency.
-   * @returns Mocked<TDependency> The mocked object corresponding to the provided
-   * string-based token.
-   */
-  get<TDependency>(token: string): VitestMocked<TDependency>;
+    /**
+     * Retrieves a reference to the mocked object of a dependency corresponding to a string-based
+     * token.
+     *
+     * @template TDependency The type of the dependency being retrieved.
+     * @param token The string-based token representing the dependency.
+     * @returns Mocked<TDependency> The mocked object corresponding to the provided
+     * string-based token.
+     */
+    get<TDependency>(token: string): VitestMocked<TDependency>;
 
-  /**
-   * Retrieves a reference to the mocked object of a dependency corresponding to its
-   * string-based identifier and the identifier metadata.
-   *
-   * @since 2.1.0
-   * @template TDependency The type of the dependency being retrieved.
-   * @param token The string-based token representing the dependency.
-   * @param identifierMetadata An accompanying metadata object for the token identifier.
-   * @returns Mocked<TDependency> The mocked object corresponding to the provided
-   * string-based token and identifier metadata.
-   */
-  get<TDependency>(
-    token: string,
-    identifierMetadata: IdentifierMetadata
-  ): VitestMocked<TDependency>;
+    /**
+     * Retrieves a reference to the mocked object of a dependency corresponding to its
+     * string-based identifier and the identifier metadata.
+     *
+     * @since 2.1.0
+     * @template TDependency The type of the dependency being retrieved.
+     * @param token The string-based token representing the dependency.
+     * @param identifierMetadata An accompanying metadata object for the token identifier.
+     * @returns Mocked<TDependency> The mocked object corresponding to the provided
+     * string-based token and identifier metadata.
+     */
+    get<TDependency>(
+      token: string,
+      identifierMetadata: IdentifierMetadata
+    ): VitestMocked<TDependency>;
 
-  /**
-   * Retrieves a reference to the mocked object of a dependency corresponding to a symbol-based
-   * token.
-   *
-   * @since 2.0.0
-   * @template TDependency The type of the dependency being retrieved.
-   * @param token The symbol-based token representing the dependency.
-   * @returns Mocked<TDependency> The mocked object corresponding to the provided
-   * symbol-based token.
-   */
-  get<TDependency>(token: symbol): VitestMocked<TDependency>;
+    /**
+     * Retrieves a reference to the mocked object of a dependency corresponding to a symbol-based
+     * token.
+     *
+     * @since 2.0.0
+     * @template TDependency The type of the dependency being retrieved.
+     * @param token The symbol-based token representing the dependency.
+     * @returns Mocked<TDependency> The mocked object corresponding to the provided
+     * symbol-based token.
+     */
+    get<TDependency>(token: symbol): VitestMocked<TDependency>;
 
-  /**
-   * Retrieves a reference to the mocked object of a dependency corresponding to its
-   * symbol-based identifier and the identifier metadata.
-   *
-   * @since 2.1.0
-   * @template TDependency The type of the dependency being retrieved.
-   * @param token The symbol-based token representing the dependency.
-   * @param identifierMetadata An accompanying metadata object for the token identifier.
-   * @returns Mocked<TDependency> The mocked object corresponding to the provided
-   * symbol-based token and identifier metadata.
-   */
-  get<TDependency>(
-    token: symbol,
-    identifierMetadata: IdentifierMetadata
-  ): VitestMocked<TDependency>;
+    /**
+     * Retrieves a reference to the mocked object of a dependency corresponding to its
+     * symbol-based identifier and the identifier metadata.
+     *
+     * @since 2.1.0
+     * @template TDependency The type of the dependency being retrieved.
+     * @param token The symbol-based token representing the dependency.
+     * @param identifierMetadata An accompanying metadata object for the token identifier.
+     * @returns Mocked<TDependency> The mocked object corresponding to the provided
+     * symbol-based token and identifier metadata.
+     */
+    get<TDependency>(
+      token: symbol,
+      identifierMetadata: IdentifierMetadata
+    ): VitestMocked<TDependency>;
 
-  /**
-   * Retrieves a reference to the mocked object of a dependency corresponding to its
-   * type, string-based or symbol-based identifier and the identifier metadata if present.
-   *
-   * This method provides flexibility in retrieving dependencies by allowing various identifier
-   * types.
-   *
-   * @since 2.1.0
-   * @template TDependency The type of the dependency being retrieved.
-   * @param identifier The type or token that the dependency corresponds to.
-   * @param identifierMetadata
-   * @returns Mocked<TDependency> The mocked object corresponding to the provided
-   * identifier, along with any available identifier metadata.
-   */
-  get<TDependency>(
-    identifier: Type<TDependency> | string | symbol,
-    identifierMetadata?: IdentifierMetadata
-  ): VitestMocked<TDependency>;
+    /**
+     * Retrieves a reference to the mocked object of a dependency corresponding to its
+     * type, string-based or symbol-based identifier and the identifier metadata if present.
+     *
+     * This method provides flexibility in retrieving dependencies by allowing various identifier
+     * types.
+     *
+     * @since 2.1.0
+     * @template TDependency The type of the dependency being retrieved.
+     * @param identifier The type or token that the dependency corresponds to.
+     * @param identifierMetadata
+     * @returns Mocked<TDependency> The mocked object corresponding to the provided
+     * identifier, along with any available identifier metadata.
+     */
+    get<TDependency>(
+      identifier: Type<TDependency> | string | symbol,
+      identifierMetadata?: IdentifierMetadata
+    ): VitestMocked<TDependency>;
+  }
 }
 
-export const stub = vi.fn();
 export * from './mock.static';
 export type Mocked<TType> = VitestMocked<TType>;
 

d585d4992b8f42c54851b9dfd108a89113f16fde
omermorad <omer.moradd@gmail.com>
2024-03-31 07:18:39 +0300

    vitest postinstall to module

--- packages/doubles/vitest/postinstall.js
+++ packages/doubles/vitest/postinstall.js
@@ -1,5 +1,9 @@
 import fs from 'node:fs';
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const filePath = path.join(__dirname, '..', 'unit', 'typings.d.ts');
 const newContent = "\nexport { mock, Mocked } from '@suites/doubles.vitest';\n";

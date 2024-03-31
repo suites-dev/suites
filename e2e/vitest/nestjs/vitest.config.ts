002b5f113392f73f4c0465c43c883cabb03000be
omermorad <omer.moradd@gmail.com>
2024-03-31 18:24:08 +0300

    e2e vitest config

--- e2e/vitest/nestjs/vitest.config.ts
+++ e2e/vitest/nestjs/vitest.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     // This is required to build the test files with SWC
     swc.vite({
       // Explicitly set the module type to avoid inheriting this value from a `.swcrc` config file
-      module: { type: 'es6' },
+      module: { type: 'nodenext' },
     }),
   ],
 });

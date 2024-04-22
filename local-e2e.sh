7bd237e52a6ac4104c91bf4574b64dd3c16b1e15
omermorad <omer.moradd@gmail.com>
2024-04-22 15:13:37 +0300

    initial e2e

--- local-e2e.sh
+++ local-e2e.sh
@@ -75,12 +75,12 @@ echo "Cleaning source packages.."
 git rm -rf packages
 
 # Test Matrix
-setup_and_test sinon nestjs
-setup_and_test sinon inversify
+#setup_and_test sinon nestjs
+#setup_and_test sinon inversify
 setup_and_test jest nestjs
-setup_and_test jest inversify
-setup_and_test vitest nestjs
-setup_and_test vitest inversify
+#setup_and_test jest inversify
+#setup_and_test vitest nestjs
+#setup_and_test vitest inversify
 
 echo -e "🎉 Testing complete!"
 

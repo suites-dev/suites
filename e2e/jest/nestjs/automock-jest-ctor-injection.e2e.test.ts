89d7bc124f73b726ac1f56fe598d406358243e09
omermorad <omer.moradd@gmail.com>
2024-06-23 15:56:20 +0300

    e2e fix

--- e2e/jest/nestjs/automock-jest-ctor-injection.e2e.test.ts
+++ e2e/jest/nestjs/automock-jest-ctor-injection.e2e.test.ts
@@ -1,18 +1,14 @@
-import type { UnitReference, Mocked } from '@suites/unit';
+import type { UnitReference, Mocked, Stub } from '@suites/unit';
 import { TestBed } from '@suites/unit';
 import type { Logger, TestClassFive } from './e2e-assets';
 import {
   ClassThatIsNotInjected,
-  Foo,
   NestJSTestClass,
   SymbolToken,
-  SymbolTokenSecond,
   TestClassFour,
   TestClassOne,
-  TestClassThree,
   TestClassTwo,
 } from './e2e-assets';
-import Mock = jest.Mock;
 
 describe('Suites Jest / NestJS E2E Test Ctor', () => {
   let unit: NestJSTestClass;
@@ -23,22 +19,20 @@ describe('Suites Jest / NestJS E2E Test Ctor', () => {
       NestJSTestClass
     )
       .mock(TestClassOne)
-      .using((stub: Mock) => ({
-        async foo(): Promise<string> {
-          return stub().mockResolvedValue('foo-from-test');
-        },
+      .using((stub: Stub) => ({
+        foo: stub().mockResolvedValue('foo-from-test'),
         bar(): string {
           return 'bar';
         },
       }))
-      .fake<string>('CONSTANT_VALUE')
-      .using('arbitrary-string')
-      .fake('UNDEFINED')
-      .using({ method: () => 456 })
+      .mock<string>('CONSTANT_VALUE')
+      .final('arbitrary-string')
+      .mock('UNDEFINED')
+      .final({ method: () => 456 })
       .mock<Logger>('LOGGER')
-      .using((stubbed: Mock) => ({ log: () => stubbed.mockReturnValue('baz-from-test') }))
-      .fake<TestClassFive>(SymbolToken)
-      .using({ doSomething: () => 'mocked' })
+      .using((stub) => ({ log: stub().mockReturnValue('baz-from-test') }))
+      .mock<TestClassFive>(SymbolToken)
+      .final({ doSomething: () => 'mocked' })
       .compile();
 
     unitRef = ref;
@@ -52,16 +46,7 @@ describe('Suites Jest / NestJS E2E Test Ctor', () => {
 
     test('then successfully resolve the dependencies of the tested classes', () => {
       expect(() => unitRef.get<{ log: () => void }>('LOGGER')).toBeDefined();
-      // expect(() => unitRef.get('UNDEFINED')).toBeDefined();
-      // expect(() => unitRef.get('UNDEFINED_SECOND')).toBeDefined();
-      // expect(() => unitRef.get(TestClassFour)).toBeDefined();
-      // expect(() => unitRef.get(TestClassThree)).toBeDefined();
-      // expect(() => unitRef.get(Foo)).toBeDefined();
-      // expect(() => unitRef.get(TestClassTwo)).toBeDefined();
-      // expect(() => unitRef.get('CONSTANT_VALUE')).toBeDefined();
       expect(() => unitRef.get(TestClassOne)).toBeDefined();
-      // expect(() => unitRef.get(SymbolToken)).toBeDefined();
-      // expect(() => unitRef.get(SymbolTokenSecond)).toBeDefined();
     });
 
     test('call the unit instance method', async () => {
@@ -72,12 +57,6 @@ describe('Suites Jest / NestJS E2E Test Ctor', () => {
       const result = await unit.test();
       expect(result).toBe('context-baz-from-test-bar');
     });
-
-    // test('then do not return the actual reflected dependencies of the injectable class', () => {
-    //   expect(() => unitRef.get(TestClassOne)).not.toBeInstanceOf(TestClassOne);
-    //   expect(() => unitRef.get(TestClassTwo)).not.toBeInstanceOf(TestClassTwo);
-    //   expect(() => unitRef.get(SymbolToken)).not.toBeInstanceOf(TestClassFive);
-    // });
 
     test('then mock the implementation of the dependencies', async () => {
       const testClassOne: Mocked<TestClassOne> = unitRef.get(TestClassOne);
@@ -105,18 +84,18 @@ describe('Suites Jest / NestJS E2E Test Ctor', () => {
 
     test('then mock the undefined reflected values and tokens', () => {
       const testClassFour: Mocked<TestClassFour> = unitRef.get(TestClassFour);
-      const undefinedValue: Mocked<{ method: () => number }> = unitRef.get<{
-        method: () => number;
-      }>('UNDEFINED');
 
       testClassFour.doSomething.mockReturnValue('mocked');
 
       expect(testClassFour.doSomething()).toBe('mocked');
-      expect(undefinedValue.method()).toBe(456);
     });
 
     test('then throw an error when trying to resolve not existing dependency', () => {
       expect(() => unitRef.get(ClassThatIsNotInjected)).toThrow();
+    });
+
+    test('then throw an error when trying to resolve faked dependency', () => {
+      expect(() => unitRef.get('UNDEFINED')).toThrow(/as it is marked as a faked dependency/);
     });
   });
 });

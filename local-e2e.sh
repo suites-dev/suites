960b64afecdb76d2b5569ad352fba598e6f084a7
omermorad <omer.moradd@gmail.com>
2024-06-24 15:50:49 +0300

    e2e

--- local-e2e.sh
+++ local-e2e.sh
@@ -85,9 +85,9 @@ git rm -rf packages
 #setup_and_test sinon nestjs
 #setup_and_test sinon inversify
 setup_and_test jest nestjs
-#setup_and_test jest inversify
-#setup_and_test vitest nestjs
-#setup_and_test vitest inversify
+setup_and_test jest inversify
+setup_and_test vitest nestjs
+setup_and_test vitest inversify
 
 echo -e "🎉 Testing complete!"
 

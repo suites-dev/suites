5929de2c31fc31aa58ecf86b06948734b322460c
Omer Morad <omer.moradd@gmail.com>
2025-08-18 18:08:33 +0300

    chore: upgrade dependencies, upgrade eslint (#802)

---
+++ eslint.config.mjs
@@ -0,0 +1,50 @@
+// @ts-check
+
+import js from '@eslint/js';
+import typescript from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,ts}'],
+    languageOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+      parser: typescriptParser,
+      parserOptions: {
+        project: true
+      },
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+        ...globals.browser,
+        Console: 'readonly'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': typescript
+    },
+    rules: {
+      ...typescript.configs.recommended.rules,
+      'no-useless-constructor': 'off',
+      'no-unused-expressions': 'off',
+      'no-redeclare': 'off',
+      'no-dupe-class-members': 'off',
+      '@typescript-eslint/no-redeclare': 'off',
+      '@typescript-eslint/no-dupe-class-members': 'error',
+      '@typescript-eslint/no-unsafe-declaration-merging': 'off',
+      '@typescript-eslint/no-namespace': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          prefer: 'type-imports',
+          disallowTypeAnnotations: false
+        }
+      ]
+    }
+  }
+];

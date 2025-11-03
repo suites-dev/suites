64815b87b4131994803a3dd741108d37b93222c7
Omer Morad <omer.moradd@gmail.com>
2025-11-03 23:12:29 +0200

    Update local-e2e.sh

--- local-e2e.sh
+++ local-e2e.sh
@@ -8,8 +8,7 @@ show_spinner() {
   while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
     local temp=${spinstr#?}
     printf " [%c]  " "$spinstr"
-    local spinstr=$temp${spinstr%"$te
-    mp"}
+    local spinstr=$temp${spinstr%"$temp"}
     sleep $delay
     printf "\b\b\b\b\b\b"
   done

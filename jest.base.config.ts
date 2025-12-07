9f8ca1f3c7b146548e123491d8438cc07795587b
Iddan Aaronsohn <mail@aniddan.com>
2025-12-07 19:22:35 +0200

    fix: add Jest moduleNameMapper for workspace dependencies

--- jest.base.config.ts
+++ jest.base.config.ts
@@ -1,3 +1,6 @@
+const path = require('path');
+const packagesDir = path.join(__dirname, 'packages');
+
 const config = (coverageDir?: string) => ({
   preset: 'ts-jest',
   roots: ['<rootDir>'],
@@ -11,6 +14,17 @@ const config = (coverageDir?: string) => ({
         isolatedModules: true,
       },
     ] as any,
+  },
+  moduleNameMapper: {
+    '^@suites/core.unit(.*)$': path.join(packagesDir, 'core/src$1'),
+    '^@suites/types.common(.*)$': path.join(packagesDir, 'types/common/src$1'),
+    '^@suites/types.di(.*)$': path.join(packagesDir, 'types/di/src$1'),
+    '^@suites/types.doubles(.*)$': path.join(packagesDir, 'types/doubles/src$1'),
+    '^@suites/di.nestjs(.*)$': path.join(packagesDir, 'di/nestjs/src$1'),
+    '^@suites/di.inversify(.*)$': path.join(packagesDir, 'di/inversify/src$1'),
+    '^@suites/doubles.jest(.*)$': path.join(packagesDir, 'doubles/jest/src$1'),
+    '^@suites/doubles.sinon(.*)$': path.join(packagesDir, 'doubles/sinon/src$1'),
+    '^@suites/doubles.vitest(.*)$': path.join(packagesDir, 'doubles/vitest/src$1'),
   },
   coveragePathIgnorePatterns: ['/node_modules/', 'spec-assets.ts', '/__test__/'],
   testPathIgnorePatterns: ['/node_modules/', '/e2e/'],

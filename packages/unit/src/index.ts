2f8c939dfd139fe2aacb77a09fc0611329c6246a
Omer Morad <omer.moradd@gmail.com>
2026-05-06 20:00:31 +0300

    fix(doubles.jest, doubles.sinon, doubles.vitest): remove redundant @suites/unit module augmentation (#1018)

--- packages/unit/src/index.ts
+++ packages/unit/src/index.ts
@@ -2,9 +2,7 @@ export { TestBed } from './testbed.js';
 export type {
   UnitTestBed,
   TestBedBuilder,
-  Mocked,
   SolitaryTestBedBuilder,
   SociableTestBedBuilder,
 } from './types.js';
 export type { UnitReference, MockOverride } from '@suites/core.unit';
-export type { Stub } from '@suites/types.doubles';
